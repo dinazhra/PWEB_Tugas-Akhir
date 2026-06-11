04142b11f72dfed3a79a0dc5394f7a911c4a6ce7
dinazhra <hafizahdiniazahra@gmail.com>
2026-06-11 21:01:53 +0700

    Fix HTTPS assets

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,9 +3,19 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-    public function register(): void {}
-    public function boot(): void {}
+    public function register(): void
+    {
+        //
+    }
+
+    public function boot(): void
+    {
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
+    }
 }
